50933ee1c3bb39a626c7cce4947541d15f65576c
kartik-modi <58023826+kartik-modi@users.noreply.github.com>
2021-06-26 16:36:40 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>JavaScript and React.js</h1>
+      <p>A Basic web development bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
